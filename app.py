f0fc6dc3eba3ca7251578a2e6351b87a3567b833
Yonis Hassan <hyonis@fb.com>
2021-03-23 16:42:34 -0700

    finished the main assignment

--- app.py
+++ app.py
@@ -44,26 +44,27 @@ app = Flask(__name__)
 def welcome():
     return """<html>
     <h1>Hawaii Climate App </h1>
-        <p>Precipitation Analysis:</p>
-            <ul>
-                <li><a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a></li>
-            </ul>
-        <p>Station Analysis:</p>
-            <ul>
-                <li><a href="/api/v1.0/stations">/api/v1.0/stations</a></li>
-            </ul>
-        <p>Temperature Analysis:</p>
-            <ul>
-                <li><a href="/api/v1.0/tobs">/api/v1.0/tobs</a></li>
-            </ul>
-        <p>Start Day Analysis:</p>
-            <ul>
-                <li><a href="/api/v1.0/2017-03-14">/api/v1.0/2017-03-14</a></li>
-            </ul>
-        <p>Start & End Day Analysis:</p>
-            <ul>
-                <li><a href="/api/v1.0/2017-03-14/2017-03-28">/api/v1.0/2017-03-14/2017-03-28</a></li>
-            </ul>
+        <p><strong>Precipitation Analysis:</strong></p>
+        
+             <a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a>
+            
+        <p><strong>Station Analysis:</strong></p>
+           
+            <a href="/api/v1.0/stations">/api/v1.0/stations</a>
+            
+        <p><strong>Temperature Analysis:</strong></p>
+
+       
+            <a href="/api/v1.0/tobs">/api/v1.0/tobs</a>
+          
+        <p><strong>Start Day Analysis:</strong></p>
+           
+            <a href="/api/v1.0/2017-03-14">/api/v1.0/2017-03-14</a>
+            
+        <p><strong>Start & End Day Analysis:</strong></p>
+           
+            <a href="/api/v1.0/2017-03-14/2017-03-28">/api/v1.0/2017-03-14/2017-03-28</a>
+          
     </html>
     """
 
@@ -77,41 +78,72 @@ def precipitation():
 
     prcp_df = pd.DataFrame(prcp, columns=['date', 'prcp'])
 
+     # Convert to list
     prcp_dic = prcp_df.to_dict()
 
     return jsonify(prcp_dic)
 
 
+##code below is from climate.ipynb file!!##
+
+# Station Route
+@app.route("/api/v1.0/stations")
+def stations():
+
+        results = session.query(Station.station, Station.name).all()
+
+        station_list = list(results)
+
+        return jsonify(station_list)
 
 
-# # Station Route
-# @app.route("/api/v1.0/stations")
-# def stations():
-#         print("station info")
-#         return 
+
+
+# TOBs Route
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    previous_year = dt.date(2017,8,23) - dt.timedelta(days = 365)
+
+    temp = session.query(Measurement.tobs).filter(Measurement.date >= previous_year).filter(Measurement.station == 'USC00519281')
+
+    order_temp = temp.order_by(Measurement.date).all()
+
+   
+    tobs_list = list(order_temp)
+
+    return jsonify(tobs_list)
 
 
 
+# Start Day Route
+@app.route("/api/v1.0/<start>")
+def start_day(start):
 
-# # TOBs Route
-# @app.route("/api/v1.0/tobs")
-# def tobs():
-#     print("print tobs info here")
-#     return 
+    start_day = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))
+
+ 
+    str_day = start_day.filter(Measurement.date >= start).group_by(Measurement.date).all()
+    
+    start_list = list(str_day)
+        
+   
+    return jsonify(start_list)
 
 
-# # Start Day Route
-# @app.route("/api/v1.0/<start>")
-# def start_day(start):
-#     print("start_day info here")
-#     return
-
-
-# # Start to End Route
-# @app.route("/api/v1.0/<start>/<end>")
-# def start_to_end(start, end):
-#     print('start and end info here')
-#     return 
+# Start to End Route
+@app.route("/api/v1.0/<start>/<end>")
+def start_to_end(start, end):
+    
+    str_to_end = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))
+    
+    str_end = str_to_end.filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).all()
+        
+   
+    start_end_list = list(str_end)
+        
+       
+    return jsonify(start_end_list)
 
 
 #define main behavior
